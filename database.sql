26a5af891c150701e975da49a66c2c9eada3d4db
Paul Girardin <paul.girardin@eemi.com>
2018-04-17 10:13:48 +0200

    Add database structure file

---
+++ database.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 17 avr. 2018 à 08:11
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `pourpre`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `badge`
+--
+
+DROP TABLE IF EXISTS `badge`;
+CREATE TABLE IF NOT EXISTS `badge` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `meeting`
+--
+
+DROP TABLE IF EXISTS `meeting`;
+CREATE TABLE IF NOT EXISTS `meeting` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `creator` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(255) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `blood_type` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `can_donate` enum('0','1') CHARACTER SET utf8 DEFAULT '1',
+  `avatar` varchar(25) CHARACTER SET utf8 DEFAULT NULL,
+  `token` varchar(255) NOT NULL,
+  `sponsor_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_badge`
+--
+
+DROP TABLE IF EXISTS `user_badge`;
+CREATE TABLE IF NOT EXISTS `user_badge` (
+  `user_id` int(11) NOT NULL,
+  `badge_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`badge_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_meeting`
+--
+
+DROP TABLE IF EXISTS `user_meeting`;
+CREATE TABLE IF NOT EXISTS `user_meeting` (
+  `user_id` int(11) NOT NULL,
+  `meeting_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`meeting_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
